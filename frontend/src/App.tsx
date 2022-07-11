dd1663889e59bce57a1bbf4d8989d2ffc9412159
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2022-07-11 16:22:33 -0300

    react

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import NotificationButton from "./components/notificationButton"
+import Header from "./components/header"
+import SalesCard from "./components/salesCard"
+
+
+function App() {
+  return(
+    <>
+    <Header/>
+    <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard/>
+        </div>
+      </section>
+    </main>
+    </>
+  )
+}
+
+export default App
