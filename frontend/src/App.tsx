4421d25c35c70ca2582041f8cfca571edcae6d9e
Eduardo Cardoso <educardosorv.carmo@gmail.com>
2022-07-17 20:18:35 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import NotificationButton from "./components/notificationButton"
 import Header from "./components/header"
 import SalesCard from "./components/salesCard"
@@ -6,6 +8,7 @@ import SalesCard from "./components/salesCard"
 function App() {
   return(
     <>
+    <ToastContainer/>
     <Header/>
     <main>
       <section id="sales">
